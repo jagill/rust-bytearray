bbdc1ea0c1a9ebeeadbd0bdf81da2aa0d694df38
James A. Gill <jagill@fb.com>
2019-12-13 17:40:37 -0500

    Handle more edge cases

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,11 @@
+use std::any::type_name;
 use std::mem;
-use zerocopy::{AsBytes, FromBytes};
+use zerocopy::{AsBytes, FromBytes, LayoutVerified};
 
 pub fn to_byte_vec<T: AsBytes>(mut v: Vec<T>) -> Vec<u8> {
+    if v.is_empty() {
+        return Vec::new();
+    }
     let p = v.as_mut_ptr();
     let len = v.len();
     let cap = v.capacity();
@@ -14,25 +18,36 @@ pub fn to_byte_vec<T: AsBytes>(mut v: Vec<T>) -> Vec<u8> {
 }
 
 pub fn from_byte_vec<T: FromBytes>(mut v: Vec<u8>) -> Result<Vec<T>, String> {
-    let p = v.as_mut_ptr();
+    let size = mem::size_of::<T>();
+    if size == 0 {
+        return Err(format!(
+            "Cannot construct size 0 vector (type {}) from bytes.",
+            type_name::<T>()
+        ));
+    }
+    if v.is_empty() {
+        return Ok(Vec::new());
+    }
     let len = v.len();
     let cap = v.capacity();
-    let size = mem::size_of::<T>();
 
-    if len % size != 0 {
-        return Err(format!(
-            "Byte vector {} {} must be a multiple of {}",
-            "length", len, size
-        ));
-    }
     if cap % size != 0 {
         return Err(format!(
-            "Byte vector {} {} must be a multiple of {}",
-            "capacity", cap, size
+            "Byte vector capacity {} must be a multiple of {} size: {}",
+            cap,
+            type_name::<T>(),
+            size
         ));
     }
 
-    #[allow(clippy::cast_ptr_alignment)]
+    LayoutVerified::<&[u8], [T]>::new_slice(v.as_slice()).ok_or_else(|| {
+        format!(
+            "Bytes are not sized or aligned for conversion to {}",
+            type_name::<T>()
+        )
+    })?;
+
+    let p = v.as_mut_ptr();
     unsafe {
         mem::forget(v);
         Ok(Vec::from_raw_parts(p as *mut T, len / size, cap / size))
--- tests/serde.rs
+++ tests/serde.rs
@@ -1,10 +1,76 @@
 use bytearray::{from_byte_vec, to_byte_vec};
 
 #[test]
-fn test_vec_u32() {
+fn test_vec_u32_roundtrip() {
     let v: Vec<u32> = vec![0, 1, 2, 3, 4, 8];
     let bytes: Vec<u8> = to_byte_vec(v.clone());
     let result: Vec<u32> = from_byte_vec(bytes).unwrap();
 
     assert_eq!(v, result);
+}
+
+#[test]
+fn test_bytes_from_vec_u32() {
+    let v: Vec<u32> = vec![0, 1, 2, 1 << 8, (1 << 16) + (1 << 31)];
+    let bytes: Vec<u8> = to_byte_vec(v);
+
+    #[rustfmt::skip]
+    assert_eq!(bytes, vec![
+        0u8, 0, 0, 0,
+        1, 0, 0, 0,
+        2, 0, 0, 0,
+        0, 1, 0, 0,
+        0, 0, 1, 128
+    ]);
+}
+
+#[test]
+fn test_bytes_to_vec_u32() {
+    let bytes = vec![
+        0u8, 0, 0, 0, 1, 0, 0, 0, 2, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 128,
+    ];
+    let v: Vec<u32> = from_byte_vec(bytes).unwrap();
+
+    assert_eq!(v, vec![0, 1, 2, 1 << 8, (1 << 16) + (1 << 31)]);
+}
+
+#[test]
+fn test_empty_vec() {
+    let v: Vec<u32> = Vec::new();
+    let bytes: Vec<u8> = to_byte_vec(v);
+    assert_eq!(bytes, vec![]);
+
+    let bytes: Vec<u8> = Vec::new();
+    let v: Vec<u32> = from_byte_vec(bytes).unwrap();
+    assert_eq!(v, vec![]);
+}
+
+#[test]
+fn test_wrong_size_bytes() {
+    #[rustfmt::skip]
+    let bytes = vec![
+        0u8, 0, 0, 0,
+        1, 0, 0, 0,
+        2, 0, 0, 0,
+        0, 1, 0, 0,
+        0, 0, 1,
+    ];
+    let result: Result<Vec<u32>, String> = from_byte_vec(bytes);
+    assert!(result.is_err());
+    assert_eq!(
+        result.unwrap_err(),
+        "Byte vector capacity 19 must be a multiple of u32 size: 4"
+    );
+}
+
+#[test]
+fn test_zero_size() {
+    #[rustfmt::skip]
+    let bytes = vec![0u8, 1, 2, 3];
+    let result: Result<Vec<()>, String> = from_byte_vec(bytes);
+    assert!(result.is_err());
+    assert_eq!(
+        result.unwrap_err(),
+        "Cannot construct size 0 vector (type ()) from bytes."
+    );
 }
